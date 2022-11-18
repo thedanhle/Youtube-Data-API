ab9e57700071b7f0ec4b12f54a37eeed41eb31e7
Danh Le <danhthanhle@usf.edu>
2022-11-18 14:27:03 -0500

    move search bar component into home component fix

--- youtube-search-app/src/app/app.module.ts
+++ youtube-search-app/src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './components/home/home.component';
+import { VideoComponent } from './components/video/video.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchBarComponent
+    HomeComponent,
+    VideoComponent
   ],
   imports: [
     BrowserModule,
